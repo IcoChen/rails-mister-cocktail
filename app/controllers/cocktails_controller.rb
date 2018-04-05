847cdbd747c67b111dac86a29303ea9ca0e23e86
Ico Chen <icelandsoundsneat@outlook.com>
2018-04-06 07:10:22 +0800

    ok done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,50 +1,28 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
-
   def index
     @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
     @cocktail = Cocktail.new
   end
 
-  def edit
-  end
-
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path
+      redirect_to cocktail_path(@cocktail)
     else
-      @cocktail.new
+      render :new
     end
   end
-end
 
+  private
 
-def update
-  if @cocktail.update(cocktail_params)
-    redirect_to cocktails_path
-  else
-    @cocktail.new
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
-
-def destroy
-  @cocktail.destroy
-end
-
-
-private
-def set_cocktail
-  @cocktail = Cocktail.find(params[:id])
-end
-
-def cocktail_params
-  params.require(:cocktail).permit(:name)
-end
-
