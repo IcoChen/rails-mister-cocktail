4c7c44f3de1ccb823eb0cd16a944a4be74ceb870
Ico Chen <icelandsoundsneat@outlook.com>
2018-04-06 11:28:09 +0800

    Prepare for Heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,16 +1,17 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [ :new, :show , :create]
+
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@dose.cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render "cocktails/show"
     end
   end
 
@@ -25,5 +26,9 @@ class DosesController < ApplicationController
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+     @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
